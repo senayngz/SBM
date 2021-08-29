5ba436bb89bd647707da5f378829863ac3892cde
Sena Dilara Yangöz <66904315+senayngz@users.noreply.github.com>
2021-08-29 14:04:43 +0300

    Add files via upload

---
+++ SBM/Insurance/src/main/java/com/example/demo/dbAccess/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.dbAccess;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.classes.User;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+	User findByTCKN(String tCKN);
+
+}
