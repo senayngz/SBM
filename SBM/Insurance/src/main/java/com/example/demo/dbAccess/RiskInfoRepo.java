3d1425548325dc7d534dcf89d298ef4ecf5ce2cf
Sena Dilara Yangöz <66904315+senayngz@users.noreply.github.com>
2021-09-07 14:41:17 +0300

    Delete SBM directory

--- SBM/Insurance/src/main/java/com/example/demo/dbAccess/RiskInfoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dbAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.classes.RiskInfo;
-
-public interface RiskInfoRepo extends JpaRepository<RiskInfo, String> {
-
-}
